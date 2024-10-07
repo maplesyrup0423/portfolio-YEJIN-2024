3e7768a9ebe8d974edaeab81e0ae418bb6b6c149
Maple <105406712+maplesyrup0423@users.noreply.github.com>
2024-10-07 12:00:16 +0900

    수정2

--- 2024portfolio-YEJIN/port2024-yejin-vite/src/assets/js/port.js
+++ 2024portfolio-YEJIN/port2024-yejin-vite/src/assets/js/port.js
@@ -6,14 +6,14 @@ export function port() {
   const horSection = gsap.utils.toArray(".port__item");
 
   gsap.to(horSection, {
-    xPercent: -120 * (horSection.length - 1),
+    xPercent: -100 * (horSection.length - 1), // 이동 거리 조정
     ease: "none",
     scrollTrigger: {
       trigger: "#port",
       start: "top 56px",
-      end: "+=3000",
+      end: "+=1500", // 스크롤 길이 조정
       pin: true,
-      scrub: 1,
+      scrub: 0.5, // 반응성 조정
       markers: false,
       invalidateOnRefresh: true,
       anticipatePin: 1,
